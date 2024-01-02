4f8f9b742b7eaf3e4ff4ec53174c79f3f6cf7bce
Raptor_Halo <119489971+Niharrnaik@users.noreply.github.com>
2024-01-02 11:50:54 +0530

    commit

--- prim.py
+++ prim.py
@@ -1,17 +1,1 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
 
-primes = []
-for num in range(0, 101):
-    if is_prime(num):
-        primes.append(num)
-
-print(primes)
-'''
-[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-'''
